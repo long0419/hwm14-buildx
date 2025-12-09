af24b8baf8c240406a772cb638dafd2b3331db3e
徐龙 <borrip0419@gmail.com>
2025-12-09 17:10:06 +0800

    update

--- src/hwm14_cli.f90
+++ src/hwm14_cli.f90
@@ -1,5 +1,5 @@
 program hwm14_cli
-    use hwm_interface, only : hwm_14      ! 注意调用的是 hwm_14
+    use hwm_interface, only : hwm_14
     implicit none
 
     real(8) :: glat, glon, alt_km
@@ -9,7 +9,6 @@ program hwm14_cli
     integer :: narg
     character(len=64) :: arg
 
-    ! 支持两种：5 参数(默认 ap=4)，或 6 参数（显式传 ap）
     narg = command_argument_count()
     if (narg < 5 .or. narg > 6) then
         print *, "Usage: hwm14_cli <lat> <lon> <alt_km> <doy> <ut_seconds> [ap]"
@@ -25,13 +24,13 @@ program hwm14_cli
     if (narg >= 6) then
         call get_command_argument(6, arg); read(arg, *) ap
     else
-        ap = 4.0d0        ! 一个常用的安静期值
+        ap = 4.0d0
     end if
 
-    ! 真正的模型调用
     call hwm_14(doy, utsec, alt_km, glat, glon, ap, w_merid, w_zonal)
 
-    ! 输出
-    write(*,'(A,2F14.6)') "Wmeridional Wzonal: ", w_merid, w_zonal
+    ! 两行输出，兼容 Python 解析：DW(1)/DW(2) 先写成 0.0 占位
+    write(*,'(A)') "Wmeridional   Wzonal   DW(1)   DW(2)"
+    write(*,'(4F14.6)') w_merid, w_zonal, 0.0d0, 0.0d0
 
 end program hwm14_cli
